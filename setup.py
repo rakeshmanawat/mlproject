c5455b12fffb0a180dbbb804db34f643399ca619
rakeshmanawat <rseervi329@gmail.com>
2025-04-06 00:22:08 +0530

    EDA and Problem statement

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     name='mlproject',
     version='0.0.1',
     author='Rakesh',
-    author_email='rseervi329@gmail.com'
+    author_email='rseervi329@gmail.com',
     packages=find_packages(),
     install_requires=get_requirements('requirements.txt')
 )
